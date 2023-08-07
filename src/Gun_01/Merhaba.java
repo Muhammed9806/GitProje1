79534960fc615afffc428bf8295e2e139a4fcb1e
Muhammed9806 <muhammedaliozata@gmail.com>
2023-08-07 19:35:41 +0300

    Task2

--- src/Gun_01/Merhaba.java
+++ src/Gun_01/Merhaba.java
@@ -3,5 +3,9 @@ package Gun_01;
 public class Merhaba {
     public static void main(String[] args) {
         System.out.println("Merhaba1");
+
+        System.out.println("Merhaba2");
+
+
     }
 }
